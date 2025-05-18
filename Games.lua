7720c39b943fb0859e65ba2d8a11029348cda862
R H <165597973+rhuda21@users.noreply.github.com>
2025-05-18 11:43:46 +0100

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,3 +1,3 @@
 getgenv().Games = {
-    [126884695634066] = "https://rhuda21.github.io/Etheral-Hub/Scripts/GrowAGarden.lua", --Grow A Garden
+    [126884695634066] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Grow%20a%20Garden.lua", --Grow A Garden
 }
