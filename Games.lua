80698da878afc7747fd07d842315edddb9d6bfdf
R H <165597973+rhuda21@users.noreply.github.com>
2025-05-24 19:00:45 +0100

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,3 +1,4 @@
 getgenv().Games = {
     [126884695634066] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Grow%20a%20Garden.lua", --Grow A Garden
+    [11276071411] = "https://rhuda21.github.io/Etheral-Hub/Scripts/BeNPCorDie.lua", --Be NPC or Die
 }
